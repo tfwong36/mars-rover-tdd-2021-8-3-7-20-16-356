fbcddd272245a5a940e13dad7531287bbac0197e
tfwong36 <tfwong36@gmail.com>
2021-12-02 16:17:35 +0800

    [MarsRover][Jason] - feat: test case for 00N move forward

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -1,10 +1,20 @@
 package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DemoTest {
     @Test
-    void write_your_first_test() {
+    void should_return_X0_Y1_when_move_given_X0_Y0_North_Move(){
+        //given
+        MarsRover marsRover = new MarsRover(0,0,'N');
+        char Command = 'M';
 
+        //when
+        marsRover.executeCommand(Command);
+        result = marsRover.getStatus();
+
+        //then
+        assertEquals("0 1 N", result);
     }
 }
