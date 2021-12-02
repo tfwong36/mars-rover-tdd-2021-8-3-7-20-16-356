9e305288a45e980e51277c6daa966c5744660213
tfwong36 <tfwong36@gmail.com>
2021-12-02 16:22:43 +0800

    [MarsRover][Jason] - fix: rename test case 0 0 N method name

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DemoTest {
     @Test
-    void should_return_X0_Y1_when_move_given_X0_Y0_North_Move(){
+    void should_return_Increment_Y_by_1_when_move_given_X0_Y0_North_Move(){
         //given
         MarsRover marsRover = new MarsRover(0,0,'N');
         char Command = 'M';
@@ -17,4 +17,5 @@ class DemoTest {
         //then
         assertEquals("0 1 N", result);
     }
+
 }
